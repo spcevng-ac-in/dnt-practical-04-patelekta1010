2e59c0b3108a6dcd51944dd32b58dcc5b10ca5e7
patelekta1010 <97274052+patelekta1010@users.noreply.github.com>
2022-04-05 09:52:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_4
+{
+    internal class Program
+    {
+        class User
+        {
+            public string Name { get; set; }
+            public string Location { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            User u = new User();
+            u.Name = "Ekta Patel";
+            u.Location = "Gujarat";
+            Console.WriteLine("Name: " + u.Name);
+            Console.WriteLine("Location: " + u.Location);
+            Console.ReadLine();
+        }
+    }
+}
